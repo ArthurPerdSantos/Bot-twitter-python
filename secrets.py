a10fd62c8a6c3844845ca41614bb5a81c6ae3477
ArthurPerdSantos <71047214+ArthurPerdSantos@users.noreply.github.com>
2021-04-17 20:32:33 -0300

    Delete secrets.py

--- secrets.py
+++
@@ -1,4 +0,0 @@
-token = '1381354637928845321-rYBg8SW9EfN7HsmqqpEqHdNAzyC6L7'
-token_secret = 'L7eVGxDoFnWRSQ7PXH2nvJUZgZtBo9t6vwmvK1T0yMDNw'
-consumer_key= 'ilsHFdkemfpcfx2IZX3HQejlE'
-consumer_secret= 'X9fflZ2BZBawyehNDqut6vv4MGObxBZPhRBs3w3U9KpG79QmWe'
