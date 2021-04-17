4a062bfef07d9019240406a81a720ede540f09ed
Arthur <arthurperdsantos@gmail.com>
2021-04-17 20:22:44 -0300

    Correção senhas

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
 import gspread
 from twitter import *
+import secrets
 
 
-token = '1381354637928845321-rYBg8SW9EfN7HsmqqpEqHdNAzyC6L7'
-token_secret = 'L7eVGxDoFnWRSQ7PXH2nvJUZgZtBo9t6vwmvK1T0yMDNw'
-consumer_key= 'ilsHFdkemfpcfx2IZX3HQejlE'
-consumer_secret= 'X9fflZ2BZBawyehNDqut6vv4MGObxBZPhRBs3w3U9KpG79QmWe'
 
 
 # chave pega no site da api do google, so renomeei
 gc = gspread.service_account('credentials.json')
 
 t = Twitter(
-    auth=OAuth(token, token_secret, consumer_key, consumer_secret))
+    auth=OAuth(secrets.token, secrets.token_secret, secrets.consumer_key, secrets.consumer_secret))
 
 # inicia um sheet, já criado, no programa
 wks = gc.open('philosophy-girl').sheet1
--- secrets.py
+++ secrets.py
@@ -1,4 +1,4 @@
-consumer_key = 'Hc4MtYanwyOROHNof3W2DX4VU'
-consumer_secret = 'Pl8dF7Mwtm3jQD2Vs4OUqPympwyw4gfWrgWSiPshlyAssLjfCI'
-token = '1381354637928845321-DcMsHCyulFED9t5YwyCSNrE3HQ6st4'
-token_secret = '0DsVNiyKaFOUH5Bv8D2zABcnaZyZdw9ldEJgWKI9R0hU4'
+token = '1381354637928845321-rYBg8SW9EfN7HsmqqpEqHdNAzyC6L7'
+token_secret = 'L7eVGxDoFnWRSQ7PXH2nvJUZgZtBo9t6vwmvK1T0yMDNw'
+consumer_key= 'ilsHFdkemfpcfx2IZX3HQejlE'
+consumer_secret= 'X9fflZ2BZBawyehNDqut6vv4MGObxBZPhRBs3w3U9KpG79QmWe'
